19eb9bc5689d76828380a958812fdca7774e4528
Sara-Rubaya <sararubaya4800@gmail.com>
2026-06-15 10:53:27 +0600

    added all queries

--- PG Local.session.sql
+++ PG Local.session.sql
@@ -75,7 +75,7 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 
 -- PART 2: SQL QUERIES
 
--- Query 1: All Champions League matches with status 'Available'
+-- Query 1: 
 SELECT
     match_id,
     fixture,
@@ -84,3 +84,58 @@ FROM Matches
 WHERE tournament_category = 'Champions League'
   AND match_status = 'Available';
 
+-- Query 2: Users whose name starts with 'Tanvir' OR contains 'Haque' (case-insensitive)
+
+
+SELECT
+    user_id,
+    full_name,
+    email
+FROM Users
+WHERE full_name ILIKE 'Tanvir%'
+   OR full_name ILIKE '%Haque%';
+
+   -- Query 3: 
+SELECT
+    booking_id,
+    user_id,
+    match_id,
+    COALESCE(payment_status, 'Action Required') AS systematic_status
+FROM Bookings
+WHERE payment_status IS NULL;
+
+-- Query 4: 
+SELECT
+    b.booking_id,
+    u.full_name,
+    m.fixture,
+    b.total_cost
+FROM Bookings b
+INNER JOIN Users   u ON b.user_id  = u.user_id
+INNER JOIN Matches m ON b.match_id = m.match_id;
+
+-- Query 5: 
+SELECT
+    u.user_id,
+    u.full_name,
+    b.booking_id
+FROM Users u
+LEFT JOIN Bookings b ON u.user_id = b.user_id
+ORDER BY u.user_id;
+
+-- Query 6:
+SELECT
+    booking_id,
+    match_id,
+    total_cost
+FROM Bookings
+WHERE total_cost > (SELECT AVG(total_cost) FROM Bookings);
+
+-- Query 7:
+SELECT
+    match_id,
+    fixture,
+    base_ticket_price
+FROM Matches
+ORDER BY base_ticket_price DESC
+LIMIT 2 OFFSET 1;
